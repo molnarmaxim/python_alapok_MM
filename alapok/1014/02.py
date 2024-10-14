365a2220a4f9cf105401a9b07eb5eb0407e6ab80
MolnarMaxim <scopsyscopsy@gmail.com>
2024-10-14 10:23:40 +0200

    Feladat elkészítve

--- alapok/1014/02.py
+++ alapok/1014/02.py
@@ -1,1 +1,10 @@
-print('sgma skbd')
+# 06_b_beagyazott_while.py - 02.py
+
+sor = 1
+while sor <= 4:
+    oszlop = 1
+    while oszlop <= 7:
+        print("O", end="")
+        oszlop += 1
+    print("")
+    sor += 1
